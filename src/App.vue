e2fdd324fd2f6ab2cc27ec0f55493db688f2bed4
nejdetkadir <nejdetkadir.550@gmail.com>
2020-11-08 22:03:49 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header></Header>
+    <Homepage></Homepage>
+  </div>
+</template>
+<script>
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage"
+export default {
+  components: {
+    Header,
+    Homepage,
+  },
+}
+</script>
