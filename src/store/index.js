e3a4473f20da0ed3caabcf3a4af9ee96232742d9
nejdetkadir <nejdetkadir.550@gmail.com>
2020-11-12 15:39:23 +0300

    Save token in store

--- src/store/index.js
+++ src/store/index.js
@@ -16,16 +16,14 @@ const store = new Vuex.Store({
     }
   },
   actions: {
-    login({state}, authData) {
-      console.log(state);
-      console.log(authData);
+    login({commit}, authData) {
       const url = authData.isUser ? `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_FIREBASE_API_KEY}` : `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_FIREBASE_API_KEY}`;
       Vue.axios.post(url, {
         email: authData.email,
         password: authData.password,
         returnSecureToken: true
       }).then((res) => {
-        console.log(res);
+        commit("setToken", res.data.idToken);
       }).catch((err) => {
         console.log(err);
       });
